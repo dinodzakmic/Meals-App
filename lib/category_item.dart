a0f430cdc62a2b4850b8be09f58a9235b1c793a9
Dino Dzakmic <dino.dzakmic@internetstores.com>
2020-02-12 12:23:23 +0100

    Added Categories with dummy data, initial redesign

---
+++ lib/category_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryItem({this.title, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          color.withOpacity(0.25),
+          color.withOpacity(0.5),
+          color.withOpacity(0.75),
+          color,
+        ], begin: Alignment.bottomLeft, end: Alignment.topRight),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
