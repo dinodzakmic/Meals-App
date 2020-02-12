48fbcb8bb6426f0e119f75f5c63e43218b98a49c
Dino Dzakmic <dino.dzakmic@internetstores.com>
2020-02-12 13:12:15 +0100

    Added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Delischz',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                fontFamily: 'RobotoCondensed',
+              ))),
       home: CategoriesScreen(),
     );
   }
