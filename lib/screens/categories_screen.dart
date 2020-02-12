f35dc4b47ae6c2bc47a0487d8fe451e19a70ccc9
Dino Dzakmic <dino.dzakmic@internetstores.com>
2020-02-12 16:43:30 +0100

    Created subfolders for Screens and Widgets

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './dummy-data.dart';
-import './category_item.dart';
+import './../dummy-data.dart';
+import './../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
