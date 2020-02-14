3cca325b4a09a55a8de646d17dfae42c9e16497a
Dino Dzakmic <dino.dzakmic@internetstores.com>
2020-02-14 16:09:42 +0100

    Updated PostCloneScript

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
-#Place this script in project/android/app/
+# place this script in project/android/app/
 cd ..
 # fail if any command fails
 set -e
 # debug log
 set -x
+
 cd ..
-git clone -b master https://github.com/flutter/flutter.git
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
+# stable - recommended for production
+
+git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
+
 flutter channel stable
 flutter doctor
-echo "Installed flutter to `pwd`/flutter"
 flutter build apk --release
-#copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
+
+# copy the APK where AppCenter will find it
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_<
